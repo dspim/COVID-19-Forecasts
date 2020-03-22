3a3955d338395af9ded51bff9c369a59983083a6
Johnson <johnson@dsp.im>
2020-03-22 17:08:20 +0800

    project init

---
+++ R/doForecast.R
@@ -0,0 +1,25 @@
+library(foreach)
+source("R/main.R")
+raw <- readRDS("historicalData2020-03-22.rds")
+
+dat <- getData(raw, country_ = "Taiwan", province_ = NA, type = "cases")
+out <- calPred(dat, endDate = as.Date("2020-03-22"))
+out
+
+
+date.stamp <- data.frame(DateRep=seq.Date(from = as.Date("2020-02-01"), to = as.Date("2020-03-21"), by = 'days'))
+out <- foreach(i=1:length(date.stamp$DateRep), .combine = rbind, .verbose = T)%do%{
+  foreach(j=1:nrow(raw), .combine = rbind)%do%{
+    dat <- getData_(raw, row = j)
+    calPred(dat, endDate = date.stamp$DateRep[i])
+  }
+}
+write.csv(out, file = "output_worldwide.csv", row.names = FALSE)
+
+
+dat <- getData(raw, country_ = "Taiwan", province_ = NA, type = "cases")
+date.stamp <- data.frame(DateRep=seq.Date(from = as.Date("2020-02-01"), to = as.Date("2020-03-21"), by = 'days'))
+out <- foreach(i=1:length(date.stamp$DateRep), .combine = rbind, .verbose = T)%do%{
+    calPred(dat, endDate = date.stamp$DateRep[i])
+}
+write.csv(out, file = "output_TW.csv", row.names = FALSE)
